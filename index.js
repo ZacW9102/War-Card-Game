d732c9799df3017f16c286fd97414acd5e0fbc7d
ZacW9102 <zacw9102@gmail.com>
2024-09-18 22:22:24 -0700

    final features

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 let deckId
+let computerScore = 0
+let myScore = 0
 const cardsContainer = document.getElementById("cards")
 const shuffleCardsBtn = document.getElementById("shuffle-cards")
 const drawCardBtn = document.getElementById("draw-cards")
 const header = document.getElementById("header")
 const remainingCards = document.getElementById("remaining-cards")
+const compScoreEl = document.getElementById("computer-score")
+const myScoreEl = document.getElementById("my-score")
 
 function handleClick() {
     fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/")
         .then(res => res.json())
-        .then(data => {
+        .then(data => { 
             deckId = data.deck_id
             remainingCards.textContent = `Remaining cards: ${data.remaining}`
         })
@@ -32,6 +36,13 @@ drawCardBtn.addEventListener("click", () => {
             
             if(data.remaining === 0) {
                 drawCardBtn.disabled = true
+                if(computerScore > myScore) {
+                    header.textContent = `The computer won the game!`
+                } else if (computerScore < myScore) {
+                    header.textContent = `You won the game!`
+                } else {
+                    header.textContent = `The game was a tie!`
+                }
             }
 
         })
@@ -43,20 +54,15 @@ function determineWinner(card1, card2) {
     const card2Value = values.indexOf(card2.value)
 
     if (card1Value > card2Value) {
-        return "Card 1 wins!"
+        myScore++
+        myScoreEl.textContent = `My Score: ${myScore}`
+        return "You win!"
     } else if  (card1Value < card2Value) {
-        return "Card 2 wins!"
+        computerScore++
+        compScoreEl.textContent = `Computer Score: ${computerScore}`
+        return "Computer wins!"
     } else {
         return "Tie!"
     }
 }
 
-const card1Obj = {
-    value: "7"
-}
-
-const card2Obj = {
-    value: "7"
-}
-
-determineWinner(card1Obj, card2Obj)
